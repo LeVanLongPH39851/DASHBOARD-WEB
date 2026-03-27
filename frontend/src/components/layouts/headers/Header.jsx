fc02576c78e7e4477b7151f73614ad7be451c55d
lvlong <lvlong@ami.vn>
2026-03-27 16:17:58 +0700

    dddhhjffff

--- frontend/src/components/layouts/headers/Header.jsx
+++ frontend/src/components/layouts/headers/Header.jsx
@@ -102,6 +102,10 @@ const Header = () => {
         }
     };
 
+    function deleteCookie(name, path = '/', domain = location.hostname) {
+        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=${path}; domain=${domain}`;
+    }
+
     const handleLogout = () => {
         deleteCookie('session');
         window.location.href = CUSTOM_CHART.domain;

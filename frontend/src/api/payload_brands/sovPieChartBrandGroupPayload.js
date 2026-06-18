2c9397a9579cbd58a1e1178c0fd751a4b589ac3b
Long <le7929590@gmail.com>
2026-06-18 09:58:32 +0700

    sdffdssdf

--- frontend/src/api/payload_brands/sovPieChartBrandGroupPayload.js
+++ frontend/src/api/payload_brands/sovPieChartBrandGroupPayload.js
@@ -247,7 +247,8 @@ export const sovPieChartBrandGroupPayload = {
       "extra_filters": [],
       "force": false,
       "result_format": "json",
-      "result_type": "full"
+      "result_type": "full",
+      heavy: true
     },
     "result_format": "json",
     "result_type": "full"
